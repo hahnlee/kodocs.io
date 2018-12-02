a46b2b9a4d11a03975e38baf1d90c976009ae840
hahnlee <hanlee.dev@gmail.com>
2018-12-02 19:20:30 +0900

    Add spinner

--- src/components/ReadMe/ReadMe.js
+++ src/components/ReadMe/ReadMe.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import gh from 'github-url-to-object';
 import octokit from '@octokit/rest';
+import Spinkit from 'react-spinkit';
 
 import 'github-markdown-css';
 
@@ -47,23 +48,35 @@ export class ReadMe extends Component {
 
   renderContent() {
     const { repository } = this.props;
-    const { data } = this.state;
+    const { isLoading, data, error } = this.state;
 
     if (!repository || !repository.url) {
       return (
-        <div>
-          :(
+        <div className={styles.messageWrapper}>
+          <h1>아직 GitHub ReadMe 정보가 없습니다 :(</h1>
         </div>
       );
     }
 
-    if (data) {
+    if (isLoading) {
       return (
-        <div className={styles.content} dangerouslySetInnerHTML={{ __html: data }} />
+        <div className={styles.spinnerWrapper}>
+          <Spinkit name="folding-cube" fadeIn="none" color="#8956FF" />
+        </div>
       );
     }
 
-    return null;
+    if (error) {
+      return (
+        <div className={styles.messageWrapper}>
+          <h1>GitHub ReadMe 정보를 가져오지 못했습니다 :(</h1>
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.content} dangerouslySetInnerHTML={{ __html: data }} />
+    );
   }
 
   render() {
